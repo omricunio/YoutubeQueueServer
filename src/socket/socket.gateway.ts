e658d0962ca2c46167081f2e6863b528640618e0
Omri Cunio <your_email@example.com>
2020-05-16 12:08:05 +0300

    .

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -1,4 +1,4 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
 import { Injectable } from '@nestjs/common';
 
@@ -7,14 +7,8 @@ import { Injectable } from '@nestjs/common';
 export class SocketGateway implements OnGatewayInit ,OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server
-
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
   
   sendMessage(roomId: string, senderId: string, eventId: string, message: any) {
-    //this.server.in(roomId).emit(eventId, message);
     for(const [socketId, socket] of Object.entries(this.server.in(roomId).sockets)) {
       if(socketId !== senderId) {
         socket.emit(eventId, message)
@@ -28,8 +22,6 @@ export class SocketGateway implements OnGatewayInit ,OnGatewayConnection, OnGate
 
   handleConnection(client: Socket) {
     const queueGuid = client.handshake.query['queueGuid'];
-    //const Authorization = client.handshake.query['Authorization'];
-    //client.id = Authorization;
     if(queueGuid !== "undefined") {
       client.join(queueGuid);
       client.emit("connected", client.id);
