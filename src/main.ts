3bc5d9320ecdf81fa802aa01a4468a5c478c6940
Omri Cunio <your_email@example.com>
2020-05-20 23:13:24 +0300

    changed port

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(8080);
+  await app.listen(80);
 }
 bootstrap();
